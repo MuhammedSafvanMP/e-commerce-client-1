fa3b0e83e9ac7ce35d0487c016475498ff974ec1
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-06-21 13:02:29 +0530

    feat: cleant-1

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+   server: {
+    host: '0.0.0.0',
+    port: 7969,
+  },
+    preview: {
+    host: '0.0.0.0',
+    port: 7969,
+    allowedHosts: [''], 
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
